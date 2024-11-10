22f916e1ce23dbf59e696466d9a8a2f928082808
JGross122 <80828463+JGross122@users.noreply.github.com>
2024-11-10 14:02:42 +0100

    Add files via upload

---
+++ code.lua
@@ -0,0 +1,300 @@
+local lobby_map = "<C><P H=\"800\" L=\"1600\" /><Z><S><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"3000\" o=\"3ba7df\" X=\"800\" c=\"4\" Y=\"399\" T=\"12\" H=\"3000\" /><S H=\"500\" L=\"191\" o=\"1b1103\" X=\"1269\" c=\"4\" Y=\"527\" T=\"12\" P=\"0,0,20,0.2,-2,0,0,0\" /><S L=\"3000\" H=\"3000\" X=\"829\" Y=\"2226\" T=\"6\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,20,0.2,28,0,0,0\" L=\"250\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"484\" T=\"12\" X=\"1090\" /><S X=\"1147\" L=\"500\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"235\" T=\"12\" P=\"0,0,20,0.2,68,0,0,0\" /><S X=\"951\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"363\" T=\"12\" P=\"0,0,20,0.2,48,0,0,0\" /><S P=\"0,0,20,0.2,-2,0,0,0\" L=\"250\" o=\"1b1103\" X=\"898\" c=\"4\" Y=\"444\" T=\"12\" H=\"35\" /><S P=\"0,0,20,0.2,118,0,0,0\" L=\"125\" o=\"1b1103\" X=\"1490\" c=\"4\" Y=\"505\" T=\"12\" H=\"10\" /><S H=\"10\" L=\"125\" o=\"1b1103\" X=\"834\" c=\"4\" Y=\"406\" T=\"12\" P=\"0,0,20,0.2,-42,0,0,0\" /><S P=\"0,0,20,0.2,28,0,0,0\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"369\" T=\"12\" X=\"825\" /><S X=\"909\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"205\" T=\"12\" P=\"0,0,20,0.2,8,0,0,0\" /><S P=\"0,0,20,0.2,-62,0,0,0\" L=\"75\" o=\"1b1103\" X=\"859\" c=\"4\" Y=\"250\" T=\"12\" H=\"10\" /><S X=\"1443\" L=\"250\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"352\" T=\"12\" P=\"0,0,20,0.2,-22,0,0,0\" /><S P=\"0,0,20,0.2,108,0,0,0\" L=\"500\" o=\"1b1103\" X=\"1362\" c=\"4\" Y=\"193\" T=\"12\" H=\"75\" /><S P=\"0,0,20,0.2,-12,0,0,0\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"156\" T=\"12\" X=\"1467\" /><S H=\"35\" L=\"250\" o=\"1b1103\" X=\"1507\" c=\"4\" Y=\"438\" T=\"12\" P=\"0,0,20,0.2,-122,0,0,0\" /><S X=\"1001\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"131\" T=\"12\" P=\"0,0,20,0.2,8,0,0,0\" /><S P=\"0,0,20,0.2,48,0,0,0\" L=\"250\" o=\"1b1103\" X=\"1244\" Y=\"148\" T=\"12\" H=\"35\" /><S H=\"10\" L=\"125\" o=\"1b1103\" X=\"1506\" c=\"4\" Y=\"101\" T=\"12\" P=\"0,0,20,0.2,118,0,0,0\" /><S P=\"0,0,20,0.2,158,0,0,0\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"97\" T=\"12\" X=\"1273\" /><S P=\"0,0,20,0.2,78,0,0,0\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"235\" T=\"12\" X=\"878\" /><S P=\"0,0,20,0.2,38,0,0,0\" L=\"75\" o=\"1b1103\" X=\"1261\" c=\"4\" Y=\"70\" T=\"12\" H=\"10\" /><S H=\"10\" L=\"75\" o=\"1b1103\" X=\"1553\" c=\"4\" Y=\"80\" T=\"12\" P=\"0,0,20,0.2,38,0,0,0\" /><S P=\"0,0,20,0.2,38,0,0,0\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"559\" T=\"12\" X=\"1503\" /><S X=\"837\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"135\" T=\"12\" P=\"0,0,20,0.2,158,0,0,0\" /><S H=\"10\" L=\"75\" o=\"1b1103\" X=\"824\" c=\"1\" Y=\"109\" T=\"12\" P=\"0,0,20,0.2,108,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" H=\"33\" Y=\"655\" T=\"12\" X=\"1154\" /><S L=\"211\" o=\"452c0a\" H=\"33\" X=\"1394\" Y=\"581\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" X=\"1092\" Y=\"538\" T=\"12\" H=\"33\" /><S L=\"211\" o=\"675c4e\" X=\"831\" H=\"33\" Y=\"425\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"675c4e\" H=\"33\" Y=\"474\" T=\"12\" X=\"1414\" /><S L=\"211\" o=\"452c0a\" H=\"33\" X=\"1176\" Y=\"373\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"675c4e\" X=\"1494\" Y=\"299\" T=\"12\" H=\"33\" /><S L=\"211\" o=\"452c0a\" X=\"919\" H=\"33\" Y=\"279\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" H=\"33\" Y=\"222\" T=\"12\" X=\"1239\" /><S L=\"211\" o=\"675c4e\" H=\"33\" X=\"941\" Y=\"144\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" X=\"1439\" Y=\"114\" T=\"12\" H=\"33\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"450\" o=\"0\" X=\"356\" Y=\"366\" T=\"12\" H=\"585\" /><S L=\"430\" o=\"1d1d1d\" X=\"356\" H=\"565\" Y=\"366\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /></S><D><DS Y=\"710\" X=\"968\" /></D><O /></Z></C>"
+
+local maps = {
+	"@7912127",
+	"@7912131",
+	"@7912143",
+	"@7912170",
+	"@7912171",
+	"@7912172",
+	"@7912333",
+	"@7912335",
+	"@7912235",
+	"@7912234",
+	"@7912232",
+	"@7912229"
+}
+
+local gameTime = 60
+local startBlocks = 50
+
+
+----------------------
+
+tfm.exec.disableAutoShaman(true)
+tfm.exec.disableAutoNewGame(true)
+tfm.exec.disableAutoTimeLeft(true)
+tfm.exec.disableAfkDeath(true)
+
+local players = {}
+
+local module = {
+	nextLevelBtnVisible = false,
+	inLobby = false
+}
+
+----------------------
+
+function calcNextLevelVote()
+	local playerNum = 0
+
+	local votedNum = 0
+
+	for nick, player in pairs(players) do
+		playerNum = playerNum + 1
+
+		if player.nextLevelVoted == true then
+			votedNum = votedNum + 1
+		end
+	end
+
+
+	local percentage = votedNum / playerNum
+
+
+	if percentage >= 0.5 then
+		startGame()
+	end
+end
+
+function condShowNextLevelBtn(nick)
+	if module.nextLevelBtnVisible then
+		local color = 0x222222
+
+		if players[nick].nextLevelVoted then
+			color = 0x225522
+		end
+
+		ui.addTextArea(3,"<a href='event:nextLevel'><font color='#FFFF00'><p align='center'>Vote next level</p></font></a>", nick, 695, 380, 100, 20, color, 0x333333, 0.8, true)
+	end
+end
+
+function hideNextLevelBtn(nick)
+	ui.removeTextArea(3, nick)
+end
+
+function configurePlayer(nick)
+	players[nick] = {
+		leftBlocks = 0,
+		infiniteBlocks = false,
+		nextLevelVoted = false
+	}
+end
+
+function resetBlocks(nick)
+	players[nick].leftBlocks = startBlocks
+	destroyBlock(nick)
+	drawLeftBlocks(nick)
+	updatePlayersStats()
+end
+
+-- How many blocks left. That text on the top-right
+function drawLeftBlocks(nick)
+	local numberOfBlocks = players[nick].leftBlocks
+
+	if players[nick].infiniteBlocks then
+		numberOfBlocks = "inf"
+	end
+
+	ui.addTextArea(0, "Objects left: "..numberOfBlocks, nick, 680, 35, 200, 50, 0, 0, 0, true)
+end
+
+-- How many blocks each player has
+---->
+function showPlayersStats(nick)
+	ui.addTextArea(1, "", nick, 10, 50, 200, 200, 0x565656, 0x767676, 0.5, true)
+	updatePlayersStats()
+end
+
+function updatePlayersStats()
+	local list = ""
+	for nick, player in pairs(players) do
+		list = list..nick..": "..player.leftBlocks.."\n"
+	end
+
+	ui.updateTextArea(1, list, nil)
+end
+
+function hidePlayersStats(nick)
+	ui.removeTextArea(1, nick)
+end
+----<
+
+-- Does player has any blocks left?
+function hasBlocks(nick)
+	return players[nick].leftBlocks > 0 or players[nick].infiniteBlocks
+end
+
+-- Place block
+function spawnBlock(nick, x, y)
+	local id = tfm.get.room.playerList[nick].id
+
+	tfm.exec.addPhysicObject(id, x, y, {
+		type = 13,
+		width = 10,
+		height = 10,
+		friction = 0,
+		resitution = 40,
+		color = math.random(0x000000, 0xffffff)
+	})
+end
+
+-- Destroy block
+function destroyBlock(nick)
+	local id = tfm.get.room.playerList[nick].id
+
+	tfm.exec.removePhysicObject(id)
+end
+
+-- Start new map and reset players' blocks
+function startGame()
+	local newMapID = math.random(1, #maps)
+	local newMap = maps[newMapID]
+	local isFlipped = math.random(0, 1) == 1
+
+	module.inLobby = false
+
+	tfm.exec.newGame(newMap, isFlipped)
+
+	for nick, player in pairs(players) do
+		player.infiniteBlocks = false
+		player.nextLevelVoted = false
+
+		hideNextLevelBtn(nick)
+	end
+
+	for nick in pairs(players) do
+		resetBlocks(nick)
+	end
+end
+
+function setGameTime()
+	tfm.exec.setGameTime(gameTime)
+end
+
+function startLobby()
+	module.inLobby = true
+
+	tfm.exec.newGame(lobby_map, false)
+
+	for nick, player in pairs(players) do
+		player.infiniteBlocks = true
+	end
+
+	for nick in pairs(tfm.get.room.playerList) do
+		drawLeftBlocks(nick)
+	end
+end
+
+-- Button showing stats of each player
+---->
+function drawStatsButton(nick, event)
+	ui.addTextArea(2,"<a href=\'event:"..event.."\'>...</a>",nick, 10, 30, 16, 15, 0x696969, 0x898989, 1, true)
+end
+
+function drawStatsButtonOn(nick)
+	drawStatsButton(nick, "statsOn")
+end
+
+function drawStatsButtonOff(nick)
+	drawStatsButton(nick, "statsOff")
+end
+----<
+
+-----------------------------
+
+function eventNewPlayer(nick)
+	configurePlayer(nick)
+	resetBlocks(nick)
+	drawLeftBlocks(nick)
+	drawStatsButtonOn(nick)
+	system.bindMouse(nick, true)
+
+	tfm.exec.respawnPlayer(nick)
+end
+
+for nick in pairs(tfm.get.room.playerList) do
+	eventNewPlayer(nick)
+end
+
+
+function eventMouse(nick, x, y)
+	-- Does player has any blocks
+	if not hasBlocks(nick) then return end
+
+	-- Decrease amount of available blocks
+	if players[nick].infiniteBlocks == false then
+		players[nick].leftBlocks = players[nick].leftBlocks - 1
+	end
+
+	spawnBlock(nick, x, y)
+
+	drawLeftBlocks(nick)
+	updatePlayersStats()
+end
+
+
+function eventPlayerDied(nick)
+	resetBlocks(nick)
+	tfm.exec.respawnPlayer(nick)
+end
+
+
+function eventTextAreaCallback(id, nick, event)
+	if event == "statsOn" then
+		drawStatsButtonOff(nick)
+		showPlayersStats(nick)
+	elseif event == "statsOff" then
+		drawStatsButtonOn(nick)
+		hidePlayersStats(nick)
+	elseif event == "nextLevel" then
+		players[nick].nextLevelVoted = true
+		condShowNextLevelBtn(nick)
+		calcNextLevelVote()
+	end
+end
+
+
+function eventPlayerWon(nick)
+	resetBlocks(nick)
+	tfm.exec.respawnPlayer(nick)
+end
+
+
+function eventChatCommand(nick, command)
+	if command == "startGame" then
+		startGame()
+	end
+end
+
+
+function eventLoop(_, timeLeft)
+	if not module.inLobby then
+		if timeLeft < 0 and timeLeft > -1000 then
+			module.nextLevelBtnVisible = true
+
+			for nick in pairs(players) do
+				condShowNextLevelBtn(nick)
+			end
+		end
+	end
+end
+
+
+function eventNewGame()
+	if not module.inLobby then
+		setGameTime()
+	end
+
+	tfm.exec.setUIMapName("Czarodziejh <BL>- Mouseour <I>1.2.0</BL></I>")
+end
+
+
+function eventPlayerLeft(nick)
+	calcNextLevelVote()
+end
+
+-----------------------------
+
+
+startLobby()
